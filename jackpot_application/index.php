d9e3b734fb5cce7e20de29d785669c3d02e0cbc3
Vuorikko Salla <s2100129@edu.bc.fi>
2021-03-30 14:34:29 +0300

    jackpot application with PHP

--- jackpot_application/index.php
+++ jackpot_application/index.php
@@ -10,14 +10,12 @@
   
 <?php
 
-$i = 0;
 $roundsTotal = 0;
 
-// while (i <= 4) {
-//   $i++;
-$rounds = 0;
+ function jackpot() {
+  $rounds = 0;
 
-  while (true) {
+    while (true) {
     $counter0 = 0;
     $counter1 = 0;
 
@@ -25,89 +23,54 @@ $rounds = 0;
 
     echo $numbers[0] . ", " . $numbers[1] . ", " . $numbers[2] . ", " . $numbers[3] . ", " . $numbers[4];
 
-    switch ($numbers[0]) {
+    for ($i = 0; $i < 5; $i++) {
+    switch ($numbers[$i]) {
       case 0:
         $counter0 += 1;
         break;
       case 1:
         $counter1 += 1;
         break;
-        default:
-        break;
+      }
     }
-    switch ($numbers[1]) {
-      case 0:
-        $counter0 += 1;
-        break;
-      case 1:
-        $counter1 += 1;
-        break;
-        default:
-        break;
-    }
-    switch ($numbers[2]) {
-      case 0:
-        $counter0 += 1;
-        break;
-      case 1:
-        $counter1 += 1;
-        break;
-      default:
-        break;
-    }
-    switch ($numbers[3]) {
-      case 0:
-        $counter0 += 1;
-        break;
-      case 1:
-        $counter1 += 1;
-        break;
-        default:
-        break;
-    }
-    switch ($numbers[4]) {
-      case 0:
-        $counter0 += 1;
-        break;
-      case 1:
-        $counter1 += 1;
-        break;
-        default:
-        break;
-    }
+ 
     $rounds += 1;
-
+    $GLOBALS['roundsTotal'] += 1;
     echo "<br>";
-    echo $counter0;
-    echo "<br>";
-    echo $counter1;
-
-
-//     rounds += 1;
-//     roundsTotal += 1;
 
     if ($counter0 == 5) {
       echo "Congratulations! You got all five 0s.";
+      echo "<br>";
     } else if (3 < $counter1 && $counter1 < 5) {
+      echo "<br>";
       echo "Congratulations! You got " . $counter1 . " 1s. You win a small prize.";
     }else if ($counter1 == 5) {
+      echo "<br>";
       echo "Congratulations! You win the jackpot! It took " . $rounds . " rounds.";
       break;
     }
   }
+}
+
+jackpot();
+jackpot();
+jackpot();
+jackpot();
+jackpot();
   
-// let roundsAverage = Math.round(roundsTotal / 5);
-// let cost = roundsAverage * 0.5
+$average = round($roundsTotal / 5);
+$money = $average * 0.5;
+$profit = 30000 - $money;
 
-// console.log(
-//   "It takes " + roundsAverage + " rounds on average to win the jackpot. " + roundsAverage +
-//   " rounds cost " + cost + " €.");
+echo "<br>";
+echo "It takes {$average} rounds on average to win the jackpot. {$average} rounds cost {$money} €." ;
+echo "<br>";
 
-//   if (cost > 30000) {
-//       console.log("You make " + (cost - 30000) + " € loss.")
-//   } else {
-//       console.log("You make " + (30000 - cost) + " € profit.")
-//   }
+  if ($money > 30000) {
+      echo "You make {$profit} € loss.";
+  } else {
+      echo "You make {$profit} € profit.";
+  }
 
   ?>
   </body>

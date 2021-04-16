da94455f146feb957d5fb722fd2992861b32e61d
Vuorikko Salla <salla.vuorikko@gmail.com>
2021-04-16 13:26:08 +0300

    fullstack_pokemon_list views 50 pokemons per page and navigation with next & previous buttons

---
+++ fullstack_pokemon_list/formatted_pokemon.php
@@ -0,0 +1,32 @@
+<?php
+    $page_number = $_GET['page'];
+
+    $data = file_get_contents('data.json');
+
+    $formatted_data = json_decode($data, true);
+    $results = $formatted_data['results'];
+
+    if ($page_number >= 0 && $page_number < (ceil(count($results)/50))) {
+        $formatted_results = array();
+
+    for ($i = 0; $i < count($results); $i++){
+
+        $formatted_results[$i]['name'] = strtoupper($results[$i]['name']);
+        $formatted_results[$i]['url'] = $results[$i]['url'];
+    }
+
+    $chunked_formatted_results = array_chunk($formatted_results, 50);
+    $results_to_be_returned = $chunked_formatted_results[$page_number];
+
+    $json_formatted_results = json_encode($results_to_be_returned);
+    echo $json_formatted_results;
+
+    $write_file_result = file_put_contents('formatted_data.json', $json_formatted_results);
+
+} else {
+    $error_response = array('status' => 500, 'message' => 'An error has occurred');
+    $json_error_response = json_encode($error_response);
+    echo $json_error_response;
+}
+
+?>
---
+++ fullstack_pokemon_list/index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pokemon list</title>
+</head>
+
+<body>
+    <button id="previous" class="navigate">Previous</button>
+    <button id="next" class="navigate">Next</button>
+    <br>
+    <br>
+    <section id="pokemonlist">
+    </section>
+
+
+    <script>
+
+        let current_page = 0;
+        fetchNewPokemons(current_page);
+        let last_page = 22, first_page = 0;
+
+        document.querySelectorAll('.navigate').forEach(button => {
+            button.addEventListener('click', (e) => {
+                console.log(e.target.id);
+                getPokemon(e.target.id);
+            })
+        })
+
+        function getPokemon(direction) {
+            if (direction === 'next' && current_page !== last_page) {
+                current_page++;
+                fetchNewPokemons(current_page);
+            } else if (direction === 'previous' && current_page !== first_page) {
+                current_page--;
+                fetchNewPokemons(current_page);
+            }
+        }
+
+        function fetchNewPokemons(page_number) {
+        
+        document.getElementById("pokemonlist").innerHTML = "";
+
+        fetch('formatted_pokemon.php?page=' + page_number, {
+                method: "GET"
+            })
+            .then((response) => response.json())
+            .then((json) => {
+                json.forEach(display_list)
+            });
+
+        function display_list(pokemon) {
+            const newEl = document.createElement("div");
+            newEl.className = "pokemon";
+            newEl.innerHTML = `<div class="name">${pokemon.name}</div><div class="url"><a href=${pokemon.url}>${pokemon.url}</a></div><br>`;
+            document.getElementById("pokemonlist").appendChild(newEl);
+        }
+    }
+
+    </script>
+
+
+</body>
+
+</html>
